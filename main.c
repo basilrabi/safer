deca7d0fede459b805822fdd46e005bb4b43e87a
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2022-08-19 21:22:29 +0800

    output status in a textfile

--- main.c
+++ main.c
@@ -2,16 +2,21 @@
 
 static void toggle_buttons(GtkWidget *button, gpointer data)
 {
+  FILE *fptr;
+  char file[30];
+  strcpy(file, data);
   if (gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (button)))
   {
-    g_print(gtk_widget_get_name(button));
-    g_print("\n");
+    fptr = fopen(strcat(file, "/status"), "w+");
+    fprintf(fptr, gtk_widget_get_name(button));
+    fprintf(fptr, "\n");
+    fclose(fptr);
   }
 }
 
 static void activate(GtkApplication* app, gpointer user_data)
 {
-  char *home_dir;
+  char *home_dir, css[30];
   GtkCssProvider *cssProvider;
   GtkWidget *box;
   GtkWidget *buttonIdling;
@@ -36,16 +41,16 @@ static void activate(GtkApplication* app, gpointer user_data)
 
   cssProvider = gtk_css_provider_new();
   home_dir = (char*)g_get_home_dir ();
-  gtk_css_provider_load_from_path(cssProvider, strcat(home_dir, "/theme.css") , NULL);
+  strcpy(css, home_dir);
+  gtk_css_provider_load_from_path(cssProvider, strcat(css, "/theme.css") , NULL);
   gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_USER);
 
-  gtk_widget_set_name(buttonIdling, "idling");
+  gtk_widget_set_name(buttonIdling, "idle");
   gtk_widget_set_name(buttonProduction, "production");
   gtk_widget_set_name(buttonQueu, "queu");
-  gtk_widget_set_name(buttonRefuelling, "refuelling");
+  gtk_widget_set_name(buttonRefuelling, "refuel");
   gtk_widget_set_name(buttonTravel, "travel");
   gtk_widget_set_name(buttonWarmup, "warmup");
-
 
   gtk_box_pack_start (GTK_BOX (box), buttonProduction, TRUE, TRUE, boxPacking);
   gtk_box_pack_start (GTK_BOX (box), buttonQueu, TRUE, TRUE, boxPacking);
@@ -54,12 +59,12 @@ static void activate(GtkApplication* app, gpointer user_data)
   gtk_box_pack_start (GTK_BOX (box), buttonIdling, TRUE, TRUE, boxPacking);
   gtk_box_pack_start (GTK_BOX (box), buttonWarmup, TRUE, TRUE, boxPacking);
 
-  g_signal_connect (buttonIdling, "clicked", G_CALLBACK (toggle_buttons), NULL);
-  g_signal_connect (buttonProduction, "clicked", G_CALLBACK (toggle_buttons), NULL);
-  g_signal_connect (buttonQueu, "clicked", G_CALLBACK (toggle_buttons), NULL);
-  g_signal_connect (buttonRefuelling, "clicked", G_CALLBACK (toggle_buttons), NULL);
-  g_signal_connect (buttonTravel, "clicked", G_CALLBACK (toggle_buttons), NULL);
-  g_signal_connect (buttonWarmup, "clicked", G_CALLBACK (toggle_buttons), NULL);
+  g_signal_connect (buttonIdling, "clicked", G_CALLBACK (toggle_buttons), home_dir);
+  g_signal_connect (buttonProduction, "clicked", G_CALLBACK (toggle_buttons), home_dir);
+  g_signal_connect (buttonQueu, "clicked", G_CALLBACK (toggle_buttons), home_dir);
+  g_signal_connect (buttonRefuelling, "clicked", G_CALLBACK (toggle_buttons), home_dir);
+  g_signal_connect (buttonTravel, "clicked", G_CALLBACK (toggle_buttons), home_dir);
+  g_signal_connect (buttonWarmup, "clicked", G_CALLBACK (toggle_buttons), home_dir);
 
   gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (buttonWarmup), TRUE);
   gtk_container_set_border_width (GTK_CONTAINER(box), 5);
