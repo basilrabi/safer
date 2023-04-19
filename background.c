3d2d099fefe3685adb8fd2de006c9ad03c0e1d94
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2023-04-19 10:20:21 +0800

    print SMS placeholder at every interval

--- background.c
+++ background.c
@@ -5,23 +5,29 @@
 
 int main(int argc, char **argv)
 {
+  int message_limit = 10;           // Number of messages to be queued before sending
+  int seconds_refresh_cutoff = 10;  // Number of seconds before the new equipment status is recorded to allow making mistakes
+  int seconds_location_period = 30; // Number of seconds between location queries
+
   char time_buffer[20];
   char *location = "LOCATION"; // Temporary placeholder for GNSS query
   char *message;
+  char *messages;
+  int buffer_size;
   int connected = 0;
-  // Number of seconds before the new equipment status is recorded to allow making mistakes
-  int seconds_refresh_cutoff = 10;
-  // Number of seconds between location queries
-  int seconds_location_period = 30;
+  int counter;
+  int total_characters;
   redisContext *context = NULL;
   redisReply *equipment_status = NULL;
   redisReply *previous_equipment_status = NULL;
   redisReply *refresh_status = NULL;
   redisReply *set_command = NULL;
+  redisReply *status_queue = NULL;
   struct tm *time_print;
   time_t current_time;
   time_t refresh_time;
 
+  // Connection Loop
   while (connected == 0) {
     context = redisConnect("localhost", 6379);
     if (context == NULL || context->err) {
@@ -37,19 +43,50 @@ int main(int argc, char **argv)
   }
 
   // TODO: Connect via serial connection
+  // TODO: Real time clock sync
 
   time(&refresh_time);
 
+  // Main loop
   while (connected == 1) {
     time(&current_time);
     equipment_status = redisCommand(context, "GET equipment_status");
     previous_equipment_status = redisCommand(context, "GET previous_equipment_status");
     refresh_status = redisCommand(context, "GET status_refresh");
+    status_queue = redisCommand(context, "LRANGE messages 0 -1");
     if (equipment_status == NULL ||
         previous_equipment_status == NULL ||
-        refresh_status == NULL) {
+        refresh_status == NULL ||
+        status_queue == NULL) {
       sd_journal_send("MESSAGE=%s", "Failed to get redis response.", "PRIORITY=%i", LOG_ERR, NULL);
       continue;
+    }
+
+    // Send SMS when the message queue limit is reached.
+    if (status_queue->type == REDIS_REPLY_ARRAY &&
+        status_queue->elements >= message_limit) {
+      total_characters = 0;
+      for (counter = 0; counter < status_queue->elements; counter++) {
+        total_characters += strlen(status_queue->element[counter]->str);
+      }
+      if (total_characters < 10 ) {
+        sd_journal_send("MESSAGE=%s", "Invalid character count.", "PRIORITY=%i", LOG_ERR, NULL);
+        continue;
+      }
+      buffer_size = total_characters + status_queue->elements;
+      messages = (char *) malloc(buffer_size * sizeof(char));
+      memset(messages, 0, buffer_size * sizeof(char));
+      strcpy(messages, status_queue->element[0]->str);
+      strcat(messages, "\n");
+      for (counter = 1; counter < status_queue->elements; counter++) {
+        strcat(messages, status_queue->element[counter]->str);
+        strcat(messages, "\n");
+      }
+      printf("All messages:\n");
+      printf("%s\n", messages);
+      set_command = redisCommand(context, "DEL messages");
+      freeReplyObject(set_command);
+      free(messages);
     }
 
     if (equipment_status->type == REDIS_REPLY_STRING) {
@@ -58,24 +95,26 @@ int main(int argc, char **argv)
         if (previous_equipment_status->type == REDIS_REPLY_STRING &&
             strcmp(previous_equipment_status->str, equipment_status->str) == 0) {
           if ((refresh_time + seconds_refresh_cutoff) <= current_time) {
-            set_command = (redisReply *) redisCommand(context, "SET status_refresh 0");
+            set_command = redisCommand(context, "SET status_refresh 0");
             freeReplyObject(set_command);
             // TODO: GNSS query
             time_print = localtime(&current_time);
             strftime(time_buffer, sizeof(time_buffer), "%Y-%m-%d-%H:%M:%S", time_print);
-            message = (char *) malloc((strlen(time_buffer) + strlen(equipment_status->str) + strlen(location) + 3) * sizeof(char));
+            buffer_size = strlen(time_buffer) + strlen(equipment_status->str) + strlen(location) + 3;
+            message = (char *) malloc(buffer_size * sizeof(char));
+            memset(message, 0, buffer_size * sizeof(char));
             strcpy(message, time_buffer);
             strcat(message, " ");
             strcat(message, equipment_status->str);
             strcat(message, " ");
             strcat(message, location);
-            set_command = (redisReply *) redisCommand(context, "RPUSH messages %s", message);
+            set_command = redisCommand(context, "RPUSH messages %s", message);
             free(message);
             freeReplyObject(set_command);
             refresh_time = current_time;
           }
         } else {
-          set_command = (redisReply *) redisCommand(context, "SET previous_equipment_status %s", equipment_status->str);
+          set_command = redisCommand(context, "SET previous_equipment_status %s", equipment_status->str);
           freeReplyObject(set_command);
           refresh_time = current_time;
         }
@@ -84,13 +123,15 @@ int main(int argc, char **argv)
           // TODO: GNSS query
           time_print = localtime(&current_time);
           strftime(time_buffer, sizeof(time_buffer), "%Y-%m-%d-%H:%M:%S", time_print);
-          message = (char *) malloc((strlen(time_buffer) + strlen(equipment_status->str) + strlen(location) + 3) * sizeof(char));
+          buffer_size = strlen(time_buffer) + strlen(equipment_status->str) + strlen(location) + 3;
+          message = (char *) malloc(buffer_size * sizeof(char));
+          memset(message, 0, buffer_size * sizeof(char));
           strcpy(message, time_buffer);
           strcat(message, " ");
           strcat(message, equipment_status->str);
           strcat(message, " ");
           strcat(message, location);
-          set_command = (redisReply *) redisCommand(context, "RPUSH messages %s", message);
+          set_command = redisCommand(context, "RPUSH messages %s", message);
           free(message);
           freeReplyObject(set_command);
           refresh_time = current_time;
@@ -101,6 +142,7 @@ int main(int argc, char **argv)
     freeReplyObject(equipment_status);
     freeReplyObject(previous_equipment_status);
     freeReplyObject(refresh_status);
+    freeReplyObject(status_queue);
   }
   return 0;
 }
