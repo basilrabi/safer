2e490c97c9250e73cb69861da1048eefe5e0b2c1
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2024-04-16 23:35:56 +0800

    remove GObject properties illustrated in tutorials
    Only signals are needed.

--- src/power-monitor.h
+++ src/power-monitor.h
@@ -11,12 +11,6 @@ G_DECLARE_FINAL_TYPE (PowerMonitor, power_monitor, SAFER, POWERMONITOR, GObject)
 
 void         power_monitor_emit_battery_level_changed(PowerMonitor *self);
 void         power_monitor_emit_voltage_level_changed(PowerMonitor *self);
-const gchar *power_monitor_get_battery               (PowerMonitor *self);
-const gchar *power_monitor_get_voltage               (PowerMonitor *self);
-void         power_monitor_set_battery               (PowerMonitor *self,
-		                                      const gchar  *batteryLevel);
-void         power_monitor_set_voltage               (PowerMonitor *self,
-		                                      const gchar  *voltageLevel);
 
 G_END_DECLS
 
