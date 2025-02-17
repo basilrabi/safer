67065ffeb61d7925b051029a532da6a6b36fcc74
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2025-02-17 16:46:53 +0800

    add path and systemd directory during install

--- install_service.sh
+++ install_service.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+path='\$HOME/bin'
+
+if ! grep -qF "$path" ~/.bashrc; then
+    echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+fi
+
+mkdir -p $HOME/.config/systemd/user
 sed "s/\$HOME/$(echo $HOME | sed 's/\//\\\//g')/g" python/bat.service > $HOME/.config/systemd/user/bat.service
